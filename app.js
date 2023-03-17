d3c9b95c4d3d2b28f254f8949f3704bee314b640
andrescavaliere <andrescavaliere14@hotmail.com>
2023-03-17 16:43:19 +0100

    documentation of app.js and datamode.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Import - Load necessary modules of the app
 const dataModel = require(__dirname + "/dataModel.js");
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -5,43 +6,45 @@ const ejs = require("ejs");
 var _ = require("lodash");
 const mongoose = require("mongoose");
 const session = require("express-session");
-const uuid = require("uuid");
+const uuid = require("uuid"); //uui generates a random version 4 UUID (Universally Unique Identifier)
 
-const app = express();
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static("public"));
-app.locals._ = _;
+const app = express(); // creates an instance of the express web framework
+app.set("view engine", "ejs"); // sets the view engine for the app to EJS templating engine
+app.use(bodyParser.urlencoded({ extended: true })); // adds middleware that parses incoming request bodies in the urlencoded format
+app.use(bodyParser.json()); // same as above but for request bodies in the JSON format
+app.use(express.static("public")); // serves static files from the "public" dir
+app.locals._ = _; // sets a global var "_" in the app's local variables, that can be accessed in views and templates
 
 app.use(
   session({
-    secret: uuid.v4(),
-    resave: false,
-    saveUninitialized: true,
+    secret: uuid.v4(), //sets a secret key for the session using the uuid.v4()
+    resave: false, //the session should not be saved on every request
+    saveUninitialized: true, //Af new session should be created for each new client that connects to the server
     cookie: { secure: false }, // set to true if using HTTPS
   })
 );
 
-//Require authentication for all routes except login and sign up
+// * Adds a middleware function
 app.use((req, res, next) => {
+  //arrow function expression
   if (["/", "/login", "/signup"].includes(req.path)) {
-    return isAuthenticated(req, res, next);
+    //if login, signup routes are included in req.path
+    return isAuthenticated(req, res, next); //then, check if user isAuthenticated
   }
-  return requireAuth(req, res, next);
+  return requireAuth(req, res, next); //otherwise, everyother route requires auth
 });
 
-// GET ROOT
+// * ROUTE HTTP GET FOR "/"
 app.get("/", function (req, res) {
-  res.redirect("/login");
+  res.redirect("/login"); //redirect traffic to /login
 });
 
-//GET LOGIN PAGE
+// * ROUTE HTTP GET FOR LOGIN "/login"
 app.get("/login", isAuthenticated, function (req, res) {
   res.render("login");
 });
 
-//POST LOGIN PAGE
+// * ROUTE HTTP POST FOR LOGIN "/login"
 app.post("/login", function (req, res) {
   const userLogin = {
     email: req.body.email.toLowerCase(),
@@ -80,26 +83,28 @@ app.post("/login", function (req, res) {
     });
 });
 
-//GET SIGNUP PAGE
+// * ROUTE HTTP GET FOR "/signup"
 app.get("/signup", isAuthenticated, function (req, res) {
   res.render("signup");
 });
 
-//POST SIGNUP PAGE
+// * ROUTE HTTP POST FOR "/signup"
 app.post("/signup", function (req, res) {
   const newUserInput = {
-    email: req.body.email.toLowerCase(),
-    password: req.body.password,
+    email: req.body.email.toLowerCase(), //get email from the form and lowercase it
+    password: req.body.password, //get password from the form
   };
 
+  //Check if given email already exists,
   dataModel.User.findOne({ email: newUserInput.email }).then(function (
     userExist
   ) {
     let message = "";
+    //If the callback function returns a promise, it means that the user already exists
     if (userExist) {
       console.log("The User ->" + newUserInput.email + " already exists");
       message = "already-exists";
-      return res.render("signup", { message: message });
+      return res.render("signup", { message: message }); //return to the signup page, with a message that the user already exists
     }
     //If User does not exist, then create a new one
     const newUser = new dataModel.User({
@@ -108,84 +113,97 @@ app.post("/signup", function (req, res) {
     });
 
     (async () => {
-      const insertQuery = newUser.save();
-      await insertQuery
+      //immediately invoked function expression (IIFE)
+      const insertQuery = newUser.save(); //insert it into the database
+      await insertQuery //pause execution until the promise is resolved
         .then(function () {
+          //if it returns a promise, it means that the insert was successful
           console.log(
             "The User -> " + newUserInput.email + " was successfully registered"
           );
           message = "signup-successful";
-          return res.render("login", { message: message });
+          return res.render("login", { message: message }); //Render now the login page after signup
         })
         .catch(function (err) {
+          //if an error occurs, it means that the insert failed
           console.log("UPS! ERROR:" + err);
         });
     })();
   });
 });
 
-// POST LOG OUT
+// * ROUTE HTTP POST FOR "/logout"
 app.post("/logout", function (req, res) {
   req.session.destroy(function (err) {
+    //destroy the session when user logs out
     if (err) {
       console.log(err);
     } else {
-      res.clearCookie("loggedIn", true);
+      res.clearCookie("loggedIn", true); //removes the loggedIn cookie
       res.redirect("/login");
     }
   });
 });
 
-// GET WORKOUTS
+// * ROUTE HTTP GET FOR "/workouts"
 app.get("/workouts", function (req, res) {
-  const query = dataModel.Workout.find({ _userId: req.session.userId });
+  const query = dataModel.Workout.find({ _userId: req.session.userId }); //Get all workouts for the user
   query.then(function (foundWorkouts) {
     res.render("workout", {
-      workoutTitle: "workout",
+      //render workout page
+      workoutTitle: "workout", //properties passed to the EJS view
       foundWorkouts: foundWorkouts,
     });
   });
 });
 
-// POST WORKOUTS
+// * ROUTE HTTP POST FOR "/workouts"
 app.post("/workouts", function (req, res) {
-  const workoutName = req.body.workoutName;
+  const workoutName = req.body.workoutName; //get new Workout Name
   (async () => {
-    await insertWorkout(workoutName, req.session.userId);
+    //IIFE
+    await insertWorkout(workoutName, req.session.userId); //Insert it to db
     res.redirect("/workouts");
   })();
 });
 
-//DELETE OR UPDATE A WORKOUTS
+// * ROUTE HTTP POST FOR DELETE OR UPDATE A WORKOUT
 app.post("/workouts/:workoutID/modify", function (req, res) {
   const workoutID = req.params.workoutID;
   const action = req.body.action;
-  const newWorkoutName = req.body.workoutName;
+  const newWorkoutName = req.body.workoutName; //New name of the workout to be modified
 
   (async () => {
+    //IIFE
     if (action === "update") {
+      //if chosen action in the dropdown menu is edit then
       await updateWorkout(workoutID, newWorkoutName);
     } else if (action === "delete") {
+      //if chosen action in the dropdown menu is delete then
       await deleteWorkout(workoutID);
     }
-    res.redirect("/workouts");
+    res.redirect("/workouts"); //either way redirect to workouts
   })();
 });
 
-// GET EXERCISES FROM A WORKOUT
+// * ROUTE HTTP GET FOR EXERCISES
 app.get("/:workoutID/exercises", function (req, res) {
   const workoutID = req.params.workoutID;
   (async () => {
-    const exercises = await getExercisesFromWorkout(workoutID);
+    //IIFE
+    const exercises = await getExercisesFromWorkout(workoutID); //Get all the exercises from a workout
     const workoutName = await getWorkoutNameById(workoutID);
     if (exercises.length > 0) {
+      //if workout has an exercise array then
       res.render("exercise", {
+        //render the exercise.ejs and properties are passed
         workoutID: workoutID,
         workoutName: workoutName,
-        exercises: exercises,
+        exercises: exercises, //contains array of all exercises
       });
     } else {
       res.render("exercise", {
+        //It doesnt contain
         workoutID: workoutID,
         workoutName: workoutName,
         exercises: [],
@@ -194,50 +212,59 @@ app.get("/:workoutID/exercises", function (req, res) {
   })();
 });
 
-// POST AN EXERCISE
+// * ROUTE HTTP POST FOR EXERCISES
 app.post("/:workoutID/exercises", function (req, res) {
-  const exerciseName = req.body.exerciseName;
-  const workoutID = req.params.workoutID;
+  const exerciseName = req.body.exerciseName; //new name of the exercise to be inserted
+  const workoutID = req.params.workoutID; //workoutID of the workout where exercise will be inserted
   (async () => {
-    await insertExercise(exerciseName, workoutID);
+    //IIFE
+    await insertExercise(exerciseName, workoutID); //insert operation
     res.redirect("/" + workoutID + "/exercises");
   })();
 });
 
-//DELETE OR UPDATE AN EXERCISE
+// * ROUTE HTTP POST TO DELETE OR UPDATE AN EXERCISE
 app.post("/:workoutID/:exerciseID/modify", function (req, res) {
-  const exerciseID = req.params.exerciseID;
-  const workoutID = req.params.workoutID;
-  const action = req.body.action;
-  const newExerciseName = req.body.exerciseName;
+  const exerciseID = req.params.exerciseID; //id to update exercise
+  const workoutID = req.params.workoutID; //id to update workout
+  const action = req.body.action; //either edit or delete
+  const newExerciseName = req.body.exerciseName; //new name of excersise to be edited
 
   (async () => {
+    //IIFE
     if (action === "update") {
+      //If user chose edit then
       await updateExercise(exerciseID, workoutID, newExerciseName);
     } else if (action === "delete") {
+      //if user chose to delete then
       await deleteExercise(exerciseID, workoutID);
     }
     res.redirect("/" + workoutID + "/exercises");
   })();
 });
 
-// GET DAYS FROM AN EXERCISE
+// * ROUTE HTTP GET FOR DAYS FROM AN EXERCISE
 app.get("/:workoutID/:exerciseID/days", function (req, res) {
-  const workoutID = req.params.workoutID;
-  const exerciseID = req.params.exerciseID;
+  const workoutID = req.params.workoutID; //get from params the workoutID
+  const exerciseID = req.params.exerciseID; //get from params the exerciseID
 
   (async () => {
-    const days = await getDaysFromExercise(exerciseID);
-    const exerciseName = await getExerciseNameById(exerciseID);
+    //IIFE
+    const days = await getDaysFromExercise(exerciseID); //Get all days from an /workout/exercise
+    const exerciseName = await getExerciseNameById(exerciseID); //Get the name of current exercise
     if (days.length > 0) {
+      //if days exists
       res.render("days", {
+        //render day.ejs with them
         workoutID: workoutID,
         exerciseID: exerciseID,
         exerciseName: exerciseName,
         days: days,
       });
     } else {
+      //if not
       res.render("days", {
+        //render day.ejs but empty days array
         workoutID: workoutID,
         exerciseID: exerciseID,
         exerciseName: exerciseName,
@@ -247,33 +274,37 @@ app.get("/:workoutID/:exerciseID/days", function (req, res) {
   })();
 });
 
-// POST DAYS INTO AN EXERCISE
+// * ROUTE HTTP POST FOR ADDING DAYS INTO AN EXERCISE
 app.post("/:workoutID/:exerciseID/days", function (req, res) {
   const exerciseID = req.params.exerciseID;
   const workoutID = req.params.workoutID;
 
   const day = new dataModel.Day({
-    date: getDate(req.body.date),
+    //Retrieve all inputs from the form
+    date: getDate(req.body.date), //get date in appropiate format
     sets: req.body.sets,
     reps: req.body.reps,
     weight: req.body.weight,
   });
 
   (async () => {
-    await insertDay(workoutID, exerciseID, day);
+    //IIFE
+    await insertDay(workoutID, exerciseID, day); //insert day
     res.redirect("/" + workoutID + "/" + exerciseID + "/days");
   })();
 });
 
-//DELETE OR UPDATE A DAY
+// * ROUTE HTTP POST TO DELETE OR UPDATE A DAY
 app.post("/:workoutID/:exerciseID/:dayID/modify", function (req, res) {
   const exerciseID = req.params.exerciseID;
   const workoutID = req.params.workoutID;
-  const dayID = req.params.dayID;
-  const action = req.body.action;
+  const dayID = req.params.dayID; //get from params the Id of day to edit or remove
+  const action = req.body.action; //either edit or remove
 
   (async () => {
+    //IIFE
     if (action === "update") {
+      //edit day with new inputs
       let day = {
         _id: dayID,
         date: getDate(req.body.date),
@@ -283,13 +314,19 @@ app.post("/:workoutID/:exerciseID/:dayID/modify", function (req, res) {
       };
       await updateDay(workoutID, exerciseID, day);
     } else if (action === "delete") {
+      //delete day
       await deleteDay(workoutID, exerciseID, dayID);
     }
     res.redirect("/" + workoutID + "/" + exerciseID + "/days");
   })();
 });
 
-// insert a workout object into database
+/**
+ * Insert a Workout into workoutsDB
+ *
+ * @param {string} workoutName
+ * @param {string} userId _id of the user inserting the workout
+ */
 async function insertWorkout(workoutName, userId) {
   const workout = new dataModel.Workout({
     name: workoutName,
@@ -307,18 +344,36 @@ async function insertWorkout(workoutName, userId) {
     });
 }
 
+/**
+ * Retrieve all exercises from a specific workout
+ *
+ * @param {string} workoutId _id of the workout
+ * @returns {exercises[]} array of exercises
+ */
 async function getExercisesFromWorkout(workoutID) {
   const query = dataModel.Workout.findOne({ _id: workoutID });
   const workout = await query;
   return workout.exercises;
 }
 
+/**
+ * Retrieve the name of a specific workout
+ *
+ * @param {string} workoutId _id of the workout
+ * @returns {string} Name of the workout
+ */
 async function getWorkoutNameById(workoutID) {
   const query = dataModel.Workout.findOne({ _id: workoutID });
   const workout = await query;
   return workout.name;
 }
 
+/**
+ * Create, Insert an exercise to workoutsDB
+ *
+ * @param {string} exerciseName name of the new exercise
+ * @param {string} workoutID workout _id where exercise is pushed to
+ */
 async function insertExercise(exerciseName, workoutID) {
   //Create and insert exercise
   const exercise = new dataModel.Exercise({
@@ -333,18 +388,37 @@ async function insertExercise(exerciseName, workoutID) {
   await workout.save();
 }
 
+/**
+ * Retrieve all days from a specific exercise
+ *
+ * @param {string} exerciseID exercise _id where days are pulled from
+ * @returns {days[]} array of days
+ */
 async function getDaysFromExercise(exerciseID) {
   const query = dataModel.Exercise.findOne({ _id: exerciseID });
   const exercise = await query;
   return exercise.days;
 }
 
+/**
+ * Retrieve the name of an exercise, using exerciseID
+ *
+ * @param {string} exerciseID _id of the exercise
+ * @returns {string} name of the exercise
+ */
 async function getExerciseNameById(exerciseID) {
   const query = dataModel.Exercise.findOne({ _id: exerciseID });
   const exercise = await query;
   return exercise.name;
 }
 
+/**
+ * Create, insert Day into workoutsDB
+ *
+ * @param {string} workoutId _id of the workout, where day is pushed to
+ * @param {string} array _id of the exercise, where day is pushed to
+ * @param {dataModel.Day} day Day object to be inserted
+ */
 async function insertDay(workoutID, exerciseID, day) {
   try {
     console.log("--- Trying to insert day with ID: " + day._id + "---");
@@ -374,6 +448,13 @@ async function insertDay(workoutID, exerciseID, day) {
   }
 }
 
+/**
+ * Update Day from workoutsDB
+ *
+ * @param {string} workoutId _id of the workout, where day will be updated from
+ * @param {string} exerciseID _id of the exercise, where day will be updated from
+ * @param {dataModel.Day} day Day object to be updated
+ */
 async function updateDay(workoutID, exerciseID, day) {
   // Update Day
   await dataModel.Day.updateOne(
@@ -427,6 +508,13 @@ async function updateDay(workoutID, exerciseID, day) {
   });
 }
 
+/**
+ * Delete Day from workoutsDB
+ *
+ * @param {string} workoutId _id of the workout, where day will be deleted from
+ * @param {string} exerciseID _id of the exercise, where day will be deleted from
+ * @param {dataModel.Day} day Day object to be deleted
+ */
 async function deleteDay(workoutID, exerciseID, dayID) {
   // delete day
   const deleteDayQuery = dataModel.Day.deleteOne({ _id: dayID });
@@ -495,6 +583,11 @@ async function deleteDay(workoutID, exerciseID, dayID) {
     });
 }
 
+/**
+ * Delete Workout from workoutsDB
+ *
+ * @param {string} workoutId _id of the workout to be deleted
+ */
 async function deleteWorkout(workoutID) {
   const deleteQuery = dataModel.Workout.deleteOne({ _id: workoutID });
   await deleteQuery
@@ -506,6 +599,12 @@ async function deleteWorkout(workoutID) {
     });
 }
 
+/**
+ * Update name of a Workout from workoutsDB
+ *
+ * @param {string} workoutId _id of the workout to be updated
+ * @param {string} workoutName new Name of the workout
+ */
 async function updateWorkout(workoutID, workoutName) {
   const updateQuery = dataModel.Workout.updateOne(
     { _id: workoutID },
@@ -520,6 +619,12 @@ async function updateWorkout(workoutID, workoutName) {
     });
 }
 
+/**
+ * Delete Exercise from workoutsDB
+ *
+ * @param {string} exerciseID _id of the exercise to be deleted
+ * @param {string} workoutId _id of the workout, where exercise will be pulled from
+ */
 async function deleteExercise(exerciseID, workoutID) {
   // delete exercise
   const deleteQuery = dataModel.Exercise.deleteOne({ _id: exerciseID });
@@ -560,6 +665,13 @@ async function deleteExercise(exerciseID, workoutID) {
     });
 }
 
+/**
+ * Update Exercise from workoutsDB
+ *
+ * @param {string} exerciseID _id of the exercise to be updated
+ * @param {string} workoutId _id of the workout, where exercise will be updated
+ * @param {string} newName new Name of the exercise
+ */
 async function updateExercise(exerciseID, workoutID, newName) {
   // update exercise
   const updateExerciseQuery = dataModel.Exercise.updateOne(
@@ -605,13 +717,31 @@ async function updateExercise(exerciseID, workoutID, newName) {
     });
 }
 
+/**
+ * Get date in the desire format. DD/MM/YYYY
+ *
+ * @param {date} date
+ * @returns {string} Date in DD/MM/YYYY format
+ */
 function getDate(date) {
   const day = new Date(date);
   const options = { year: "numeric", month: "numeric", day: "numeric" };
   return day.toLocaleDateString("en-GB", options);
 }
 
-//Middleware function
+/**
+ * *Middleware Section
+ */
+
+/**
+ * Checks if user is logged in,
+ * if true, redirects to "/Workouts", else next()
+ *
+ * @param {??} req
+ * @param {??} res
+ * @param {??} next
+ * @returns {??}
+ */
 function isAuthenticated(req, res, next) {
   if (req.session && req.session.userId) {
     return res.redirect("/workouts");
@@ -620,18 +750,26 @@ function isAuthenticated(req, res, next) {
   }
 }
 
-// Middleware to check if user is authenticated
+/**
+ * Check if user is logged in,
+ * if true, allows access to whatever requested Route,
+ * if not authenticated, redirects to "/Login"
+ *
+ * @param {} req HTTP Request
+ * @param {} res HTTP Response
+ * @param {} next Next Middleware function in the stack
+ * @returns {}
+ */
 function requireAuth(req, res, next) {
   if (req.session && req.session.userId) {
     // User is authenticated
-    return next();
+    return next(); //Pass control to the next middleware function in the stack
   } else {
-    // User is not authenticated
-    return res.redirect("/login");
+    return res.redirect("/login"); //If user is not authenticated, redirect to "/Login"
   }
 }
 
-//LISTEN PORT 3000
+// * Set express app to listen on port 3000
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
--- dataModel.js
+++ dataModel.js
@@ -1,12 +1,17 @@
+// Import Mongoose Module
 const mongoose = require("mongoose");
-// Connect to the workouts database
+
+// Connect to workoutsDB
 const workoutsDB = mongoose.createConnection(
   "mongodb://127.0.0.1:27017/workoutsDB"
 );
-
-// Connect to the  users database
+// Connect to usersDB
 const usersDB = mongoose.createConnection("mongodb://127.0.0.1:27017/usersDB");
 
+/**
+ * * Create schemas for day, exercise, workouts
+ * * These schemas are for the workoutsDB
+ */
 const daySchema = new mongoose.Schema({
   date: {
     type: String,
@@ -46,13 +51,18 @@ const workoutSchema = new mongoose.Schema({
   },
 });
 
-module.exports.Workout = workoutsDB.model("Workout", workoutSchema);
-module.exports.Exercise = workoutsDB.model("Exercise", exerciseSchema);
-module.exports.Day = workoutsDB.model("Day", daySchema);
+/**
+ * * Create schemas user
+ * * This schema is for the usersDB
+ */
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
 });
 
+// * Link each Schema to a model in its respective DB and export it as a module
+module.exports.Workout = workoutsDB.model("Workout", workoutSchema);
+module.exports.Exercise = workoutsDB.model("Exercise", exerciseSchema);
+module.exports.Day = workoutsDB.model("Day", daySchema);
 module.exports.User = usersDB.model("User", userSchema);
